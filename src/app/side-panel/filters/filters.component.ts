42955300f6184a2a26c3b6a9f8109247fe6bbe25
lauren.oldham@ursaspace.com <lauren.oldham@uky.edu>
2020-10-07 21:04:19 -0400

    Two way data binding between map and form

--- src/app/side-panel/filters/filters.component.ts
+++ src/app/side-panel/filters/filters.component.ts
@@ -37,66 +37,72 @@ export class FiltersComponent implements OnInit {
 
   ngOnInit(): void {
     this.dataService.setConflictData(this.conflicts.features);
-
-    // console.log(this.conflicts);
-
-    this.form = this.fb.group({
-      countries: [[], Validators.required],
-      actors: [[], Validators.required],
-      fatalities: [[], Validators.required],
-    });
-
-    // this.dataService.conflictData$.subscribe((data: FeatureCollection) => {
-    //   if (data) {
-    //     this.conflicts = data;
-
-        // console.log(this.conflicts);
-
-    const fatalities = this.conflicts.features.map(x => +x.properties.incident_fatalities);
-    const min = Math.min(...fatalities);
-    const max = Math.max(...fatalities);
-    // console.log(max);
-    this.options.floor = min;
-    this.options.ceil = max;
-    this.form.controls.fatalities.patchValue([min, max]);
+    //
+    // // console.log(this.conflicts);
+    //
+    // this.form = this.fb.group({
+    //   countries: [[], Validators.required],
+    //   actors: [[], Validators.required],
+    //   fatalities: [[], Validators.required],
+    // });
+    //
+    // // this.dataService.conflictData$.subscribe((data: FeatureCollection) => {
+    // //   if (data) {
+    // //     this.conflicts = data;
+    //
+    //     // console.log(this.conflicts);
+    //
+    // const fatalities = this.conflicts.features.map(x => +x.properties.incident_fatalities);
+    // const min = Math.min(...fatalities);
+    // const max = Math.max(...fatalities);
+    // // console.log(max);
+    // this.options.floor = min;
+    // this.options.ceil = max;
+    // this.form.controls.fatalities.patchValue([min, max]);
 
     this.conflicts.features.forEach(feature => {
       this.countries.push(feature.properties.country);
-      this.actors.push(feature.properties.actor1, feature.properties.actor2);
+      // this.actors.push(feature.properties.actor1, feature.properties.actor2);
     });
 
     // TODO create master toggle, remove '' option from actors
     // TODO use filter on this.conflicts.features instead of looping?
     this.countries = [...new Set(this.countries.sort())];
-    this.actors = [...new Set(this.actors.sort())];
-    // console.log(this.actors);
-
+    // this.actors = [...new Set(this.actors.sort())];
+    // // console.log(this.actors);
+    //
     this.dataService.applyFilters(this.filters);
-
-    this.dataService.filters$.subscribe(f => {
-      // console.log(f);
-      this.filters.countries = f.countries;
-      this.form.controls.countries.setValue(this.filters.countries);
-      console.log(this.form.controls.countries.value);
-    });
-
-  }
-
-  onchange(e) {
-    // console.log(e);
-  }
-
-  change(): void {
-    // console.log(this.form.controls.countries.value);
-    // console.log('change');
-
-    // console.log(countries);
-    this.filters.countries = this.form.controls.countries.value;
-    this.dataService.applyFilters(this.filters);
-    // countries.forEach(country => {
-    //   const y = this.conflicts.features.filter(x => x.properties.country === country);
-    //   console.log(y);
+    //
+    // this.dataService.filters$.subscribe(f => {
+    //   // console.log(f);
+    //   this.filters.countries = f.countries;
+    //   this.form.controls.countries.setValue(this.filters.countries);
+    //   console.log(this.form.controls.countries.value);
     // });
+
   }
+
+  selectCountry(e) {
+    console.log(e);
+    this.filters.countries.push(e);
+    this.dataService.applyFilters(this.filters);
+  }
+  //
+  // onchange(e) {
+  //   // console.log(e);
+  // }
+  //
+  // change(): void {
+  //   // console.log(this.form.controls.countries.value);
+  //   // console.log('change');
+  //
+  //   // console.log(countries);
+  //   this.filters.countries = this.form.controls.countries.value;
+  //   this.dataService.applyFilters(this.filters);
+  //   // countries.forEach(country => {
+  //   //   const y = this.conflicts.features.filter(x => x.properties.country === country);
+  //   //   console.log(y);
+  //   // });
+  // }
 
 }
